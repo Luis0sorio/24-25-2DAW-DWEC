2f6d71519a6cfd933caa1d81d61fd8749a1dd606
SergioFontan <sergiofontanllamas@gmail.com>
2024-10-11 20:48:24 +0200

    Clase 11-OCT

---
+++ clases/2024-10-11. Ejercicios coches (III)/enunciado.js
@@ -0,0 +1,568 @@
+let coches = {
+    'coche 1': {
+        marca: "Honda",
+        modelo: "Civic",
+        anio: 2020,
+        color: "rojo",
+        precio: 25000,
+        carateristicas: [
+            "transmision automatica",
+            "techo corredizo",
+            "sistema de navegacion",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "John",
+        edad: 35,
+        localizacion: "New York",
+        },
+    },
+    'coche 2': {
+        marca: "Chevrolet",
+        modelo: "Camaro",
+        anio: 2019,
+        color: "negro",
+        precio: 30000,
+        carateristicas: [
+        "transmision manual",
+        "asientos de piel",
+        "sistema de sonido premium",
+        ],
+        enVenta: false,
+        propietario: {
+        nombre: "Samantha",
+        edad: 28,
+        localizacion: "Los Angeles",
+        },
+    },
+    'coche 3': {
+        marca: "Tesla",
+        modelo: "Model S",
+        anio: 2021,
+        color: "plata",
+        precio: 100000,
+        carateristicas: [
+        "motor electrico",
+        "conduccion autonoma",
+        "doble motor de traccion total",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "Mike",
+        edad: 40,
+        localizacion: "San Francisco",
+        },
+    },
+    'coche 4': {
+        marca: "Volkswedadn",
+        modelo: "Golf",
+        anio: 2018,
+        color: "azul",
+        precio: 20000,
+        carateristicas: [
+        "turbocharged engine",
+        "techo corredizo panoramico",
+        "control de crucero adaptativo",
+        ],
+        enVenta: false,
+        propietario: {
+        nombre: "Emily",
+        edad: 32,
+        localizacion: "Chicago",
+        },
+    },
+    'coche 5': {
+        marca: "Toyota",
+        modelo: "Corolla",
+        anio: 2020,
+        color: "blanco",
+        precio: 22000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "David",
+        edad: 29,
+        localizacion: "Houston",
+        },
+    },
+    'coche 6': {
+        marca: "",
+        modelo: "Focus",
+        anio: 2022,
+        color: "gris",
+        precio: 24000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        "monitoreo de punto ciego",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "Alex",
+        edad: 31,
+        localizacion: "Seattle",
+        },
+    },
+    'coche 7': {
+        marca: "Ford",
+        modelo: "Mustang",
+        anio: 2020,
+        color: "amarillo",
+        precio: 35000,
+        carateristicas: [
+        "transmision manual",
+        "asientos de piel",
+        "sistema de sonido premium",
+        "modo sport",
+        ],
+        enVenta: false,
+        propietario: {
+        nombre: "Karen",
+        edad: 26,
+        localizacion: "Boston",
+        },
+    },
+    'coche 8': {
+        marca: "Hyundai",
+        modelo: "Tucson",
+        anio: 2021,
+        color: "naranja",
+        precio: 30000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        "control de crucero adaptativo",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "Chris",
+        edad: 33,
+        localizacion: "Dallas",
+        },
+    },
+    'coche 9': {
+        marca: "Volvo",
+        modelo: "XC90",
+        anio: 2020,
+        color: "marron",
+        precio: 60000,
+        carateristicas: [
+        "all-wheel drive",
+        "asientos de piel",
+        "panoramic techo corredizo",
+        "suspension adaptativa",
+        ],
+        enVenta: false,
+        propietario: {
+        nombre: "Sarah",
+        edad: 38,
+        localizacion: "Miami",
+        },
+    },
+    'coche 10': {
+        marca: "Mitsubishi",
+        modelo: "Outlander",
+        anio: 2019,
+        color: "verde",
+        precio: 35000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        "control de crucero adaptativo",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "Jason",
+        edad: 30,
+        localizacion: "Atlanta",
+        },
+    },
+    'coche 11': {
+        marca: "Nissan",
+        modelo: "Leaf",
+        anio: 2021,
+        color: "morado",
+        precio: 45000,
+        carateristicas: [
+        "motor electrico",
+        "conduccion autonoma",
+        "control de crucero adaptativo",
+        "pantalla tactil",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "Katie",
+        edad: 26,
+        localizacion: "Philadelphia",
+        },
+    },
+    'coche 12': {
+        marca: "Toyota",
+        modelo: "Rav4",
+        anio: 2022,
+        color: "rosa",
+        precio: 30000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        "monitoreo de punto ciego",
+        ],
+        enVenta: false,
+        propietario: {
+        nombre: "Matt",
+        edad: 29,
+        localizacion: "Phoenix",
+        },
+    },
+    'coche 13': {
+        marca: "Honda",
+        modelo: "Accord",
+        anio: 2020,
+        color: "rojo",
+        precio: 30000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        "control de crucero adaptativo",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "Jasmine",
+        edad: 32,
+        localizacion: "San Diego",
+        },
+    },
+    'coche 14': {
+        marca: "Ford",
+        modelo: "Escape",
+        anio: 2021,
+        color: "negro",
+        precio: 35000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        "control de crucero adaptativo",
+        ],
+        enVenta: false,
+        propietario: {
+        nombre: "Ryan",
+        edad: 29,
+        localizacion: "Denver",
+        },
+    },
+    'coche 15': {
+        marca: "Honda",
+        modelo: "CR-V",
+        anio: 2018,
+        color: "plata",
+        precio: 30000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        "control de crucero adaptativo",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "Megan",
+        edad: 27,
+        localizacion: "Detroit",
+        },
+    },
+    'coche 16': {
+        marca: "Nissan",
+        modelo: "Rogue",
+        anio: 2020,
+        color: "azul",
+        precio: 30000,
+        carateristicas: [
+        "transmision automatica",
+        "pantalla tactil",
+        "camara trasera",
+        "control de crucero adaptativo",
+        ],
+        enVenta: true,
+        propietario: {
+        nombre: "Ben",
+        edad: 35,
+        localizacion: "Minneapolis",
+        },
+    }
+};
+/**
+ * 1. Crea una función que imprima en la consola todos los coches que están en venta, mostrando su marca y modelo.
+ * 
+ * 2. Define una función que reciba un nombre de propietario y devuelva el modelo del coche que le pertenece.
+ * 
+ * 3. Escribe una función que reciba un color y un año, y devuelva un array con todos los coches que coinciden con esos criterios.
+ * 
+ * 4. Crea una función que devuelva la cantidad total de coches que están en venta.
+ * 
+ * 5. Define una función que reciba un precio y determine si hay coches en venta que sean más baratos que ese precio.
+ * 
+ * 6. Escribe una función que reciba un modelo de coche y devuelva sus características.
+ * 
+ * 7. Crea una función que reciba un año y devuelva un array con todos los coches que son más nuevos que ese año.
+ * 
+ * 8. Define una función que calcule y devuelva el precio promedio de todos los coches en venta.
+ * 
+ * 9. Escribe una función que devuelva un objeto agrupando los coches por su marca.
+ * 
+ * 10. Crea una función que imprima en la consola el nombre y la localización de cada propietario de coche que está en venta.
+ * 
+ * 11. Define una función que reciba una característica y devuelva un array de coches que la tengan.
+ * 
+ * 12. Escribe una función que calcule la suma total de los precios de todos los coches que están en venta.
+ * 
+ * 13. Crea una función que reciba un color y devuelva cuántos coches de ese color hay en el objeto.
+ * 
+ * 14. Define una función que devuelva el coche con el precio más alto que esté en venta.
+ * 
+ * 15. Escribe una función que reciba un modelo y devuelva true si ese coche está en venta, o false en caso contrario.
+ * 
+ * 16. Crea una función que devuelva un array con todos los coches del objeto coches.
+ * 
+ * 17. Define una función que reciba un modelo de coche y devuelva la información completa del coche que corresponde a ese modelo.
+ * 
+ * 18. Escribe una función que reciba un año y imprima todos los coches que son de ese año.
+ * 
+ * 19. Crea una función que reciba una marca y devuelva true si hay al menos un coche de esa marca, o false si no.
+ * 
+ * 20. Define una función que devuelva el coche más antiguo que está en venta.
+ *
+ * 21. Escribe una función que reciba un precio máximo y devuelva un array con todos los coches que son más baratos que ese precio.
+ *
+ * 22. Crea una función que imprima las características de todos los coches que están en venta.
+ *
+ * 23. Define una función que cuente cuántos propietarios diferentes hay entre los coches en venta.
+ *
+ * 24. Escribe una función que reciba un color y devuelva un array con todos los coches que son de ese color.
+ *
+ * 25. Crea una función que sume y devuelva la edad total de todos los propietarios de coches que están en venta.
+ *
+ * 26. Define una función que reciba un modelo de coche y una característica, y devuelva true si el coche tiene esa característica, o false en caso contrario.
+ *
+ * 27. Escribe una función que devuelva un objeto agrupando los coches por su año.
+ *
+ * 28. Crea una función que reciba el nombre de un coche y imprima en la consola toda su información.
+ *
+ * 29. Define una función que devuelva la cantidad de coches que tienen transmisión automática.
+ *
+ * 30. Escribe una función que reciba una edad y devuelva un array con los coches cuyos propietarios son de esa edad o más jóvenes.
+ * */
+
+
+// 9. Escribe una función que devuelva un objeto agrupando los coches por su marca.
+function ej09(){
+    let respuesta = new Object(); // Creo el objeto vacío
+
+    for(let i in coches){
+        let claves = Object.keys(respuesta); // Guarda las marcas del objeto respuesta en variable claves
+        let marca = coches[i]['marca']; // Guardo la marca del coche actual
+        // Comprobar si la marca del coche de la iteracion actual existe
+        if(claves.includes(marca)){
+            // True, implica que la marca ya la he colocado
+            respuesta[marca].push(coches[i]); // meto el coche actual
+        } else{
+            // Crear nueva marca (propiedad) con nombre de la marca y valor el coche actual
+            Object.defineProperty(respuesta, marca, {
+                value: new Array(), 
+                writable: true, // permitimos que se pueda modificar (la propiedad marca)
+                enumerable: true, // permitimos que se muestre al enumerar las propiedades
+            }); 
+            
+            // Una vez creo la marca y puedo modificarla, inserto el coche de la iteracion actual
+            respuesta[marca].push(coches[i]); // meto el coche actual
+        }
+    }
+    return respuesta; // Retorno el objeto
+}
+
+// console.log(ej09());
+
+
+// 11. Define una función que reciba una característica y devuelva un array de coches que la tengan.
+/**
+ * Funcion que recibe una caracteristica y devuelve las claves de los coches que lo contengan
+ * @param {string} caracteristica - Caracteristica del coche
+ * @returns {string|array} - String con mensaje si el tipo no es correcto. Array con claves si el tipo es correcto.
+ */
+function ej11(caracteristica){
+    if(typeof caracteristica == 'string'){
+        let response = new Array(); // creo el array que contendra los coches
+
+        for (let i in coches) {
+            if (coches[i]['carateristicas'].includes(caracteristica)) {
+                response.push(i); // En este caso introduzco la clave como respuesta            
+            }
+        }
+        return response;
+    }else{
+        return 'el tipo no es correcto';
+    }
+    
+}
+
+// console.log(ej11('techo corredizo'));
+// console.log(ej11('hola'));
+// console.log(ej11(null));
+
+// Opcion B. Sin usar include(menos puntos)
+function ej11b(caracteristica){
+    let response = new Array(); // creo el array que contendra los coches
+
+    for (let i in coches) {
+        for(let j in coches[i]['caracteristicas']){
+            if (coches[i]['caracteristicas'][j] == caracteristica) {
+                response.push(i); // En este caso introduzco la clave como respuesta            
+            }
+        }
+        
+    }
+    return response;
+}
+
+// 13. Crea una función que reciba un color y devuelva cuántos coches de ese color hay en el objeto.
+/**
+ * 
+ * @param {string} color 
+ * @returns {number}
+ */
+function ej13(color){
+    if(typeof color == 'string'){
+        let total = 0;
+        for(let i in coches){
+            if(coches[i].color == color){
+                total++;
+            }
+        }
+        return total;
+    }
+    return -1;
+}
+
+// console.log(ej13('rojo'));
+// console.log(ej13('gris'));
+// console.log(ej13('azul'));
+// console.log(ej13(null));
+
+// 16. Crea una función que devuelva un array con todos los coches del objeto coches.
+function ej16(){
+    let response = new Array();
+    for(let i in coches){
+        response.push(coches[i]);
+    }
+    return response;
+}
+
+// console.log(ej16());
+
+// 27. Escribe una función que devuelva un objeto agrupando los coches por su año.
+function ej27(){
+    let respuesta = new Object();
+
+    for(let i in coches){
+        let year = coches[i]['anio'];                   // Me quedo el año de la iteracion ACTUAL
+        let aniosRespuesta = Object.keys(respuesta);    // Guardo en la variable los años ya introducidos en la respuesta
+    //   console.log(typeof aniosRespuesta[0])
+        if(aniosRespuesta.includes(year.toString())){
+            respuesta[year].push(i);
+        }else{
+            Object.defineProperty(respuesta, year, {
+                value: new Array(i), 
+                writable: true, // permitimos que se pueda modificar (la propiedad marca)
+                enumerable: true, // permitimos que se muestre al enumerar las propiedades
+            });
+        }
+        // console.log(`año actual ${year}`);
+        // console.log(aniosRespuesta);
+        // console.log(respuesta);
+        // console.log('');
+    }
+    return respuesta;
+}
+
+console.log(ej27());
+
+// 20. Define una función que devuelva el coche más antiguo que está en venta.
+function ej20(){
+    // Respuesta es el array que me dara el resultado
+    let respuesta = [];
+
+    // Coche es el objeto con el pack que tengo que insertar
+    let coche = {
+        nombre: null,
+        anio: null
+    }
+
+    for (let i in coches){
+        if(coches[i]['enVenta']){
+
+            // Estamos en la primera iteracion
+            if(coche['anio'] == null){
+                
+                coche = {
+                    nombre: i,
+                    anio: coches[i]['anio']
+                }
+                respuesta.push(coche);
+            }
+            // Al ser el año mas pequeño, debemos BORRAR respuesta e INSERTAR el coche
+            else if(coches[i]['anio'] < respuesta[0]['anio']){
+                
+                coche = {
+                    nombre: i,
+                    anio: coches[i]['anio']
+                }
+                // Borramos array e insertamos en la primera posicion
+                respuesta= new Array();
+                respuesta.push(coche);
+            }
+            // Al ser el año IGUAL, hacemos push
+            else if(coches[i]['anio'] == respuesta[0]['anio']){
+
+                coche = {
+                    nombre: i,
+                    anio: coches[i]['anio']
+                }
+                // Insertamos en la siguiente posicion del array
+                respuesta.push(coche);
+            }
+        }
+    }
+    return respuesta;
+}
+console.log(ej20());
+
+// 21. Escribe una función que reciba un precio máximo y devuelva un array con todos los coches que son más baratos que ese precio.
+
+/**
+ * 
+ * @param {*} precio 
+ * @returns 
+ */
+function ej21(precio){
+    if(typeof precio == 'number'){
+        let response = new Array();
+        for(let i in coches){
+            if(coches[i]['precio'] < precio){
+                response.push(i);
+            }
+        }
+        return response;
+    }
+    return -1;    
+}
+
+console.log(ej21(30000));
+console.log(ej21('30000'));
